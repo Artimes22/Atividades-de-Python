76570bc94162f7df93e328c4a569a802b99101df
Artimes22 <artimes.flima@gmail.com>
2022-07-25 19:33:34 -0300

    ex 05 ate 08

---
+++ ex06.py
@@ -0,0 +1,5 @@
+n = int(input("Digite um número: "))
+d = n * 2
+t = n * 3
+r = pow(n, (1/2))
+print('Com base o valor {},\nO dobro é {}, \nO triplo {} \nSua raiz {:.2f}'.format(n, d, t, r))
---
+++ ex08.py
@@ -0,0 +1,11 @@
+from pygame import KMOD_ALT
+
+
+medida = float(input('Informe uma dsitância em metros: '))
+km = medida / 1000
+hm = medida / 100
+dam = medida / 10
+dm = medida * 10
+cm = medida * 100
+mm = medida * 1000
+print('A medida de {}m coresponde a \n{}km, \n{}hm, \n{}dam, \n{}dm, \n{}cm, \n{}mm'.format(medida, km ,hm, dam, dm, cm, mm))
