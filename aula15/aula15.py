f0075da8db5082d4b5f7a14213682218742dd721
Artimes22 <artimes.flima@gmail.com>
2022-11-23 17:09:34 -0300

    Update aula15.py

--- aula15/aula15.py
+++ aula15/aula15.py
@@ -4,4 +4,5 @@ while True:
     if n == 999:
         break
     s += n
-print('A soma dos números é igual a {}' .format(s))    
+#print('A soma dos números é igual a {}' .format(s))    
+print(f'A soma dos números é {s}')
