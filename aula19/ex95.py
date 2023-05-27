f748965ba2cce12f5db512ad5319da0bb1956c88
Artimes22 <artimes.flima@gmail.com>
2023-05-27 05:43:28 -0300

    finalizando ex95

--- aula19/ex95.py
+++ aula19/ex95.py
@@ -19,13 +19,30 @@ while True:
         print('ERRO! Responda apenas S ou N')
     if resp == 'N':
         break
+
+print('-='*22)
+print('cod', end=' ')
+for i in jogador.keys():
+    print(f'{i:<15}',end='')    
+print('')    
     
 print('='*40)    
 for k, v in enumerate(time):
-    print(f'{k:>5}', end='')
+    print(f'{k:>3}', end=' ')
     for d in v.values():
-        print(f'{str(d):>15}', end='')
+        print(f'{str(d):<15}', end='')
     print('')    
 print('='*40)
 print('-='*20)    
- 
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar) '))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRO! Não existe jogador com o codigo da {busca}.')
+    else:
+        print(f' -- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}:')     
+        for i, g in enumerate(time[busca]["gols"]):
+            print(f'      No jogo {i+1} ele marcou {g} gols.') 
+    print('='*40)        
+print('<<<VOLTE SEMPRE>>>')    
