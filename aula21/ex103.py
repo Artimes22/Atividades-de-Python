a60ec6ac9f2d4405c22792333f55b7230e71c560
Artimes22 <artimes.flima@gmail.com>
2023-05-28 15:36:00 -0300

    Create ex103.py

---
+++ aula21/ex103.py
@@ -0,0 +1,15 @@
+def ficha(jogador = '<Desconhecido>', gols = 0):
+    print(f'O jogador {jogador} fez {gols} gosls no campeonato')
+    
+j = str(input('Nome do jogador: '))
+g = str(input('Gols marcados: '))   
+
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if j.strip() == '':
+    ficha(gols=g)
+else:
+    ficha(j, g)    
+            
