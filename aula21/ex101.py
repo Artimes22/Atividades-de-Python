42640b647e381d9942af86fe00b6af5c48cfa386
Artimes22 <artimes.flima@gmail.com>
2023-05-28 15:14:48 -0300

    ex101 e 102

---
+++ aula21/ex101.py
@@ -0,0 +1,14 @@
+def voto(ano):
+    from datetime import datetime
+    atual = datetime.now().year 
+    idade = atual - ano
+    if idade < 16:
+        return f'Você tem {idade} anos, NÃO VOTA'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Você tem {idade} anos, seu voto é OPCIONAL'
+    else:
+        return f'Você tem {idade} anos, seu voto é OBRIGATORIO.'
+        
+nasc = int(input('Qual ano de nascimento: '))    
+
+print(voto(nasc))
---
+++ aula21/ex102.py
@@ -0,0 +1,21 @@
+def fatorial(num = 1, show=False):
+    """
+        ->Calcula o fatorial de um número.
+    PARA - n: o numero a ser calculado.
+    PARA - show: (opcional) mostrar ou não a conta 
+    return: O valor fatorial de n.     
+    """
+    f = 1
+    for c in range (num, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')    
+        f *= c
+        
+    return f
+
+n=(int(input('Digite um valor: ')))
+print(fatorial(n, show=True))
