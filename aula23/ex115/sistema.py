4f849cd5c8e97ed055d923d6d8aa534111ab0553
Artimes22 <artimes.flima@gmail.com>
2023-06-10 05:45:45 -0300

    finalizando ex115

--- aula23/ex115/sistema.py
+++ aula23/ex115/sistema.py
@@ -1,12 +1,25 @@
 import time
 from lib.interface import *
+from lib.arquivo import *
+
+
+arq = 'aula23/ex115/cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+         
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        print('Opção 1')
+        #Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
     elif resposta == 2:
-        print('Opção 2') 
+        #Opção para cadastrar uma nova pessoa!
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = int(input('Idade: '))
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabeçalho('Saindo do sistema... Até logo!!')
         break 
